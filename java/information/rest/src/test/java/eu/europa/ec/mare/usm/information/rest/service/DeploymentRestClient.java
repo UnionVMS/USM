85e2b1fc554ae5eea904a94598753d64ceb603aa
James Sörling <james.p.sorling@consid.se>
2017-09-11 15:40:25 +0200

    ssl trustManager jersey client

--- java/information/rest/src/test/java/eu/europa/ec/mare/usm/information/rest/service/DeploymentRestClient.java
+++ java/information/rest/src/test/java/eu/europa/ec/mare/usm/information/rest/service/DeploymentRestClient.java
@@ -14,6 +14,8 @@
  */
 package eu.europa.ec.mare.usm.information.rest.service;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
 import javax.ws.rs.core.MediaType;
 
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
@@ -24,6 +26,7 @@ import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.client.urlconnection.HTTPSProperties;
 
 import eu.europa.ec.mare.usm.information.domain.deployment.Application;
 import eu.europa.ec.mare.usm.information.service.DeploymentService;
@@ -35,109 +38,95 @@ public class DeploymentRestClient implements DeploymentService {
 	private final WebResource webResource;
 	private final Client client;
 
-	public DeploymentRestClient(String uri) 
-  {
+	public DeploymentRestClient(String uri) {
 		ClientConfig config = new DefaultClientConfig();
+
+		try {
+			config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES,
+					new HTTPSProperties(new HostnameVerifier() {
+						@Override
+						public boolean verify(String hostname, SSLSession session) {
+							return true;
+						}
+					}, DomainTrustManager.getSslContext()));
+		} catch (Exception e) {
+		}
+
 		config.getClasses().add(JacksonJsonProvider.class);
 		client = Client.create(config);
 		webResource = client.resource(uri).path("deployments");
 	}
 
+	@Override
+	public Application getDeploymentDescriptor(String request) throws IllegalArgumentException, RuntimeException {
+		ClientResponse r = webResource.path(request).type(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML)
+				.get(ClientResponse.class);
 
-  @Override
-  public Application getDeploymentDescriptor(String request) 
-  throws IllegalArgumentException,  RuntimeException 
-  {
-    ClientResponse r = webResource.path(request).
-                       type(MediaType.APPLICATION_XML).
-                       accept(MediaType.APPLICATION_XML).
-                       get(ClientResponse.class);
-    
-    Application ret = null;
-    
-    if (ClientResponse.Status.OK == r.getClientResponseStatus()) {
-      ret = r.getEntity(Application.class);
-    } else if (ClientResponse.Status.BAD_REQUEST == r.getClientResponseStatus()) {
-      if (MediaType.APPLICATION_XML_TYPE == r.getType()) {
-        StatusResponse sr = r.getEntity(StatusResponse.class);
-        throw new IllegalArgumentException(sr.getMessage());
-      }
-      throw new IllegalArgumentException();
-    } else if (ClientResponse.Status.INTERNAL_SERVER_ERROR == r.getClientResponseStatus()) {
-      if (MediaType.APPLICATION_XML_TYPE == r.getType()) {
-        StatusResponse sr = r.getEntity(StatusResponse.class);
-        throw new RuntimeException(sr.getMessage());
-      }
-      StatusResponse sr = r.getEntity(StatusResponse.class);
-      throw new RuntimeException(sr.getMessage());
-    }
-    
-    return ret;
-  }
+		Application ret = null;
 
-  @Override
-  public void deployApplication(Application request) 
-  throws IllegalArgumentException,  RuntimeException 
-  {
-    try {
-      webResource.type(MediaType.APPLICATION_XML).
-                  accept(MediaType.APPLICATION_XML).
-                  post(request);
-    } catch (UniformInterfaceException e) {
-      handleException(e);
-    }
-  }
+		if (ClientResponse.Status.OK == r.getClientResponseStatus()) {
+			ret = r.getEntity(Application.class);
+		} else if (ClientResponse.Status.BAD_REQUEST == r.getClientResponseStatus()) {
+			if (MediaType.APPLICATION_XML_TYPE == r.getType()) {
+				StatusResponse sr = r.getEntity(StatusResponse.class);
+				throw new IllegalArgumentException(sr.getMessage());
+			}
+			throw new IllegalArgumentException();
+		} else if (ClientResponse.Status.INTERNAL_SERVER_ERROR == r.getClientResponseStatus()) {
+			if (MediaType.APPLICATION_XML_TYPE == r.getType()) {
+				StatusResponse sr = r.getEntity(StatusResponse.class);
+				throw new RuntimeException(sr.getMessage());
+			}
+			StatusResponse sr = r.getEntity(StatusResponse.class);
+			throw new RuntimeException(sr.getMessage());
+		}
 
-  @Override
-  public void redeployApplication(Application request) 
-  throws IllegalArgumentException,  RuntimeException 
-  {
-    try {
-      webResource.type(MediaType.APPLICATION_XML).
-                  accept(MediaType.APPLICATION_XML).
-                  put(request);
-    } catch (UniformInterfaceException e) {
-      handleException(e);
-    }
-  }
+		return ret;
+	}
 
-  @Override
-  public void undeployApplication(String request) 
-  throws IllegalArgumentException,  RuntimeException 
-  {
-    try {
-      webResource.path(request).
-                  accept(MediaType.APPLICATION_XML).
-                  delete();
-    } catch (UniformInterfaceException e) {
-      handleException(e);
-    }
-  }
+	@Override
+	public void deployApplication(Application request) throws IllegalArgumentException, RuntimeException {
+		try {
+			webResource.type(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).post(request);
+		} catch (UniformInterfaceException e) {
+			handleException(e);
+		}
+	}
 
-  @Override
-  public void deployDatasets(Application request) 
-  throws IllegalArgumentException, RuntimeException 
-  {
-    try {
-      webResource.path("datasets").
-                  type(MediaType.APPLICATION_XML).
-                  accept(MediaType.APPLICATION_XML).
-                  put(request);
-    } catch (UniformInterfaceException e) {
-      handleException(e);
-    }
-  }
+	@Override
+	public void redeployApplication(Application request) throws IllegalArgumentException, RuntimeException {
+		try {
+			webResource.type(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).put(request);
+		} catch (UniformInterfaceException e) {
+			handleException(e);
+		}
+	}
 
-  private void handleException(UniformInterfaceException e) 
-  throws IllegalArgumentException, RuntimeException 
-  {
-    ClientResponse r = e.getResponse();
-    if (ClientResponse.Status.BAD_REQUEST == r.getClientResponseStatus()) {
-      throw new IllegalArgumentException();
-   } else if (ClientResponse.Status.INTERNAL_SERVER_ERROR == r.getClientResponseStatus()) {
-      throw new RuntimeException("");
-    }
-  }
+	@Override
+	public void undeployApplication(String request) throws IllegalArgumentException, RuntimeException {
+		try {
+			webResource.path(request).accept(MediaType.APPLICATION_XML).delete();
+		} catch (UniformInterfaceException e) {
+			handleException(e);
+		}
+	}
 
+	@Override
+	public void deployDatasets(Application request) throws IllegalArgumentException, RuntimeException {
+		try {
+			webResource.path("datasets").type(MediaType.APPLICATION_XML).accept(MediaType.APPLICATION_XML).put(request);
+		} catch (UniformInterfaceException e) {
+			handleException(e);
+		}
+	}
+
+	private void handleException(UniformInterfaceException e) throws IllegalArgumentException, RuntimeException {
+		ClientResponse r = e.getResponse();
+		if (ClientResponse.Status.BAD_REQUEST == r.getClientResponseStatus()) {
+			throw new IllegalArgumentException();
+		} else if (ClientResponse.Status.INTERNAL_SERVER_ERROR == r.getClientResponseStatus()) {
+			throw new RuntimeException("");
+		}
+	}
 
 }
--- java/information/rest/src/test/java/eu/europa/ec/mare/usm/information/rest/service/DomainTrustManager.java
+++ java/information/rest/src/test/java/eu/europa/ec/mare/usm/information/rest/service/DomainTrustManager.java
@@ -1,23 +1,17 @@
 package eu.europa.ec.mare.usm.information.rest.service;
 
-import static org.junit.Assert.*;
-
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.security.cert.X509Certificate;
-import java.util.Properties;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-import org.junit.Test;
+public class DomainTrustManager {
 
-public class DomainTrustManagerTest {
-
+	public static SSLContext sc;
+	
 	static {
 		TrustManager[] trustAllCerts = new TrustManager[] { 
 			    new X509TrustManager() {     
@@ -33,24 +27,15 @@ public class DomainTrustManagerTest {
 			    } 
 			}; 
 			try {
-			    SSLContext sc = SSLContext.getInstance("SSL"); 
+			    sc = SSLContext.getInstance("TLS"); 
 			    sc.init(null, trustAllCerts, new java.security.SecureRandom()); 
 			    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
 			} catch (GeneralSecurityException e) {
 		} 
 	}
-
-	@Test
-	public void httpsConnectTest() throws Exception {
-		InputStream is = getClass().getResourceAsStream("/test.properties");
-		Properties props = new Properties();
-		props.load(is);
-		String endPoint = props.getProperty("rest.endpoint");
-		try { 
-		    URL url = new URL(endPoint);
-		    
-		} catch (MalformedURLException e) {
-		} 
+	
+	public static SSLContext getSslContext() {
+		return sc;
 	}
 	
 }
